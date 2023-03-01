0f337f32c05f24fff9984adc5fd4ca303eb2ca08
Sang jin An <photoan79@naver.com>
2023-03-01 02:41:47 -0800

    DB user change

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -11,11 +11,11 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
